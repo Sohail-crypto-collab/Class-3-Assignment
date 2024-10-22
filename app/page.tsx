afb8a385d5b0b35bdeb7b28fba3af4efbe18babe
Muhammad Sohail Rehman <sohailrehman011@gmail.com>
2024-10-22 16:46:47 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function home() {
+  return;
+  <div>
+    <h1>Assignment No 3</h1>
+    <h4>
+      1-Create a footer and render it once so that it's available across all
+      routes.
+    </h4>
+  </div>;
+}
