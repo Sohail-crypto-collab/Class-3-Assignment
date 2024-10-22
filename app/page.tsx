6abc51579500d9c8479103e4119bb718b0e141e1
Muhammad Sohail Rehman <sohailrehman011@gmail.com>
2024-10-22 17:04:51 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// tailwindcss.com
+// Windows/Linux: Shift + Alt + F
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Assignment 3
+      </h1>
+    </div>
+  );
+}
